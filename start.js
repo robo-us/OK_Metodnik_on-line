77f2eff2f06ed24c0879160094cd010de48ba9f9
Leszek Janczewski <Leszek Janczewski>
2017-04-11 18:29:20 +0200

    dodałem odpalenie strony nauczyciela oraz stronę nauczyciela

--- start.js
+++ start.js
@@ -3,9 +3,13 @@ var path = require('path');
 var app = express();
 
 app.use(express.static('klient'));
-
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + './index.html'));
+});
+
+app.use('serwer', express.static('serwer'));
+app.get('/serwer', function(req, res) {
+	res.sendFile(path.join(__dirname + '/serwer/index.html'));
 });
 
 app.listen(3000, function() {
